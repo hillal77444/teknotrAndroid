a90c6f20219bd5f982566ba62d3005717ba39adc
egesenkul <egesenkul35@hotmail.com>
2019-11-03 23:10:23 +0300

    Drawer Eklemek
    teknotra.com adresinde paylaşmış olduğum Android Uygulama Geliştirme yazılarının 2. kısmı tamamlandı

--- app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
+++ app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
@@ -1,17 +1,49 @@
 package com.egesenkul.teknotraandroid;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         tamEkranYap();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Initialization();
+    }
+
+    private void Initialization() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.open_drawer,R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 
     protected void tamEkranYap(){
