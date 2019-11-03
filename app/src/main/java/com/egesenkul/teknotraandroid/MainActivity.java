2daed9a7ccd4aeccbf178d56338ff6bb6058154a
egesenkul <egesenkul35@hotmail.com>
2019-11-03 10:38:39 +0300

    Başlangıç Yazısı Tamamlandı
    teknotra.com adresinde paylaşmış olduğum Android Uygulama Geliştirme yazılarının 1. kısmı tamamlandı

--- app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
+++ app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
@@ -2,12 +2,20 @@ package com.egesenkul.teknotraandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        tamEkranYap();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void tamEkranYap(){
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
     }
 }
