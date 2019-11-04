8aca6200d1665d3e5712a452eb8609ee92c04e3c
egesenkul <egesenkul35@hotmail.com>
2019-11-04 20:39:29 +0300

    Drawer Event Eklemek
    teknotra.com adresinde paylaşmış olduğum Android Uygulama Geliştirme yazılarının 3. kısmı tamamlandı

--- app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
+++ app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
@@ -1,5 +1,7 @@
 package com.egesenkul.teknotraandroid;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,10 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.open_drawer,R.string.close_drawer);
@@ -49,5 +55,34 @@ public class MainActivity extends AppCompatActivity {
     protected void tamEkranYap(){
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_car:
+                Toast.makeText(getApplicationContext(), "Araba", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_mobile:
+                Toast.makeText(getApplicationContext(), "Mobil", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.software_development:
+                Toast.makeText(getApplicationContext(), "Yazılım Geliştirme", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.social_media:
+                Toast.makeText(getApplicationContext(), "Sosyal Medya", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.game:
+                Toast.makeText(getApplicationContext(), "Oyun", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.other:
+                Toast.makeText(getApplicationContext(), "Diğer", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_computer:
+                Toast.makeText(getApplicationContext(), "Bilgisayar", Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
