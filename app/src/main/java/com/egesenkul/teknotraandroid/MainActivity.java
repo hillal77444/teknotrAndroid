9f9afd8218cb3fbd4bbcb7a89ee34d34d8fedaed
egesenkul <egesenkul35@hotmail.com>
2019-11-08 20:38:15 +0300

    ListView Yenilemek İçin Kaydır
    teknotra.com adresinde paylaşmış olduğum Android Uygulama Geliştirme yazılarının 8. kısmı tamamlandı

--- app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
+++ app/src/main/java/com/egesenkul/teknotraandroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -47,6 +48,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+        final SwipeRefreshLayout pullToRefresh = findViewById(R.id.pullToRefresh);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                listeyiYenile(); 
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -59,6 +69,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 R.string.open_drawer,R.string.close_drawer);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    private void listeyiYenile() {
+        Toast.makeText(getApplicationContext(), "Listeyi yenile fonksiyonu çalıştı", Toast.LENGTH_LONG).show();
     }
 
     private void ListeTiklama(int index){
